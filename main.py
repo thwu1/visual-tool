edb83cc720b0e540182851013305cca8c189ebe8
jiaolab-tianhao <wth569749099@gmail.com>
2023-12-03 05:46:05 +0000

    add vllm backend

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import math
+import time
 
 import gradio as gr
-from utils import logprobs_from_logits, color_map, openchat_template
+import torch
+from transformers import AutoTokenizer
+from vllm import LLM, SamplingParams
+
+from utils import color_map, logprobs_from_logits, openchat_template
 
 
 class VisualizeLLM:
     def __init__(self, model_name, extra_generate_kwargs, chat_template) -> None:
-        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.llm = LLM(model=model_name)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.extra_generate_kwargs = extra_generate_kwargs
+        self.extra_generate_kwargs = extra_generate_kwargs  # deprecated
         if self.extra_generate_kwargs.get("pad_token_id") is None:
             self.extra_generate_kwargs["pad_token_id"] = self.tokenizer.eos_token_id
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.chat_template = chat_template
-
-        max_new_token_slider = gr.Slider(minimum=1, maximum=4096, step=1, label="Max New Tokens", value=256)
-        temperature_slider = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, label="Temperature", value=0.7)
-        do_sample_checkbox = gr.Checkbox(label="Do Sample", value=True)
+        max_new_token_slider = gr.Slider(minimum=1, maximum=8192, step=1, label="Max New Tokens", value=1024)
+        temperature_slider = gr.Slider(minimum=0.0, maximum=2.0, step=0.01, label="Temperature", value=0.7)
+        use_beam_search_box = gr.Checkbox(label="Use beam search", value=False)
         top_k_slider = gr.Slider(minimum=0, maximum=100, step=1, label="Top K", value=0)
         top_p_slider = gr.Slider(minimum=0, maximum=1, step=0.01, label="Top P", value=1)
-
-        # custom_css = """
-        #     .output_interface {
-        #         width: 600px; /* Set the width of the output boxes */
-        #         height: 300px; /* Set the height of the output boxes */
-        #     }
-        #     """
+        best_of_slider = gr.Slider(minimum=1, maximum=100, step=1, label="Best Of, for beam search", value=3)
 
         self.iface = gr.Interface(
-            fn=lambda *args: self.to_html_str(self.gen_and_cal_prob(*args)),
-            inputs=["text", max_new_token_slider, temperature_slider, do_sample_checkbox, top_k_slider, top_p_slider],
+            fn=lambda *args: self.to_html_str(*self.gen_and_cal_prob(*args)),
+            inputs=["text", max_new_token_slider, temperature_slider, use_beam_search_box, top_k_slider, top_p_slider, best_of_slider],
             outputs=["html", "html"],
-            # css=custom_css,
             theme="default",
             title="Visualize LLMs",
             description=f"The color visualize the log-probabilities of the generated tokens, from red (low probability) to green (high probability). Should expect the model to generate low probability if it feels uncertain. The model used is {model_name}.",
@@ -40,8 +36,6 @@ class VisualizeLLM:
 
     def prepare_input(self, text) -> dict:
         input_tensor = self.tokenizer(text)
-        for key, val in input_tensor.items():
-            input_tensor[key] = torch.tensor(val, dtype=torch.long)
         return input_tensor
 
     def cal_logprob(self, input_ids, output_ids):
@@ -61,35 +55,45 @@ class VisualizeLLM:
         logprobs = logprobs_from_logits(logits[:, :-1, :], input_kwargs["input_ids"][:, 1:])
         return {"output_tokens": output_ids[:, -num_gen_tokens:], "logprobs": logprobs[:, -num_gen_tokens:]}
 
-    def create_prob_ls(self, stats):
-        """create a list of dictionaries like [{'text': 'Hello', 'value': 0.5}, ...]"""
+    def cal_prob(self, logprobs):
+        """logprobs = [{token_id: logprob}, ...}]"""
         ls = []
-        for token, logprob in zip(stats["output_tokens"][0], stats["logprobs"][0]):
-            ls.append({"text": self.tokenizer.convert_ids_to_tokens(token.item()).replace("▁", " "), "value": torch.exp(logprob).item()})
+        for logprob in logprobs:
+            for token_id, logprob in logprob.items():
+                ls.append({"text": self.tokenizer.convert_ids_to_tokens(token_id).replace("▁", " ").replace("<0x0A>","<|new_line|>"), "value": math.exp(logprob)})
         return ls
 
-    def gen_and_cal_prob(self, text, max_new_tokens, temperature, do_sample, top_k, top_p):
+    def gen_and_cal_prob(self, text, max_new_tokens, temperature, use_beam_search, top_k, top_p, best_of):
+        time_stats = "<b>Stats:</b><br>"
         input_text = [self.chat_template(text)]
-        input_tensor = self.prepare_input(input_text)
-        output_ids = self.model.generate(
-            **input_tensor,
-            max_new_tokens=max_new_tokens,
-            temperature=temperature,
-            do_sample=do_sample,
-            top_k=top_k,
-            top_p=top_p,
-            **self.extra_generate_kwargs,
-        )
-        stats = self.cal_logprob(input_tensor["input_ids"], output_ids)
-        return self.create_prob_ls(stats)
+        input_ids = self.tokenizer(input_text)["input_ids"]
 
-    def to_html_str(self, data) -> [str, str]:
+        sampling_params = SamplingParams(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k if top_k > 0 else -1,
+            max_tokens=max_new_tokens,
+            use_beam_search=use_beam_search,
+            logprobs=1,
+            stop_token_ids=[self.tokenizer.eos_token_id],
+            best_of=best_of,
+            skip_special_tokens=False,
+        )
+
+        start = time.time()
+        outputs = self.llm.generate(prompt_token_ids=input_ids, sampling_params=sampling_params)[0].outputs[0]
+        time_stats += f"generate time: {time.time() - start:.2f} s<br>"
+
+        return self.cal_prob(outputs.logprobs), time_stats
+
+    def to_html_str(self, data, time_stats: str) -> [str, str]:
         """data is a list of dictionaries like [{'text': 'Hello', 'value': 0.5}, ...]"""
         colored_text = ""
-        legend_info = "<br><br><b>Legend:</b><br>"
+        legend_info = time_stats + "<br><b>Legend:</b><br>"
         for item in data:
             background_color = color_map(item["value"])
-            colored_text += f"<span style='background-color: {background_color}; padding: 2px;'>{item['text']}</span>"
+            text = item["text"] if item["text"] != "<|new_line|>" else " <br>"
+            colored_text += f"<span style='background-color: {background_color}; padding: 0px;'>{text}</span>"
             legend_info += f"{item['text']}: Probability: {item['value']:.2f}, Color: {background_color}<br>"
         return colored_text, legend_info
 
@@ -99,6 +103,7 @@ class VisualizeLLM:
 
 if __name__ == "__main__":
     model_name = "openchat/openchat_3.5"
-    generate_kwargs = {}
-    server = VisualizeLLM(model_name, generate_kwargs, openchat_template)
+    # model_name = "lvwerra/gpt2-imdb"
+    extra_generate_kwargs = {}
+    server = VisualizeLLM(model_name, extra_generate_kwargs, openchat_template)
     server.run()
