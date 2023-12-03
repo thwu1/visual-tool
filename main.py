49c40d25e583a676d2e5c40cef8d587cbc005fe9
jiaolab-tianhao <wth569749099@gmail.com>
2023-12-03 19:44:07 +0000

    add entropy

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import time
 import gradio as gr
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from vllm import LLM, SamplingParams, AsyncLLMEngine
+from vllm import LLM, SamplingParams
 
-from utils import color_map, logprobs_from_logits, openchat_template
+from utils import color_map, logprobs_from_logits, entropy_from_logits, openchat_template
 
 
 class VisualizeLLM:
@@ -15,37 +15,47 @@ class VisualizeLLM:
         if use_vllm:
             self.llm = LLM(model_name)
         else:
-            self.llm = AutoModelForCausalLM.from_pretrained(model_name, use_flash_attention_2=use_flash_attention_2, torch_dtype=torch.bfloat16).to("cuda")
+            self.llm = AutoModelForCausalLM.from_pretrained(model_name, use_flash_attention_2=use_flash_attention_2, torch_dtype=torch.bfloat16).to(
+                "cuda"
+            )
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.extra_generate_kwargs = extra_generate_kwargs  # deprecated
         if self.extra_generate_kwargs.get("pad_token_id") is None:
             self.extra_generate_kwargs["pad_token_id"] = self.tokenizer.eos_token_id
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.chat_template = chat_template
-        max_new_token_slider = gr.Slider(minimum=1, maximum=8192, step=1, label="Max New Tokens", value=2048)
+
+        max_new_token_slider = gr.Slider(minimum=1, maximum=8192, step=1, label="Max New Tokens", value=256)
         temperature_slider = gr.Slider(minimum=0.0, maximum=1.5, step=0.01, label="Temperature", value=0.5)
         use_beam_search_box = gr.Checkbox(label="Use beam search", value=False)
         top_k_slider = gr.Slider(minimum=0, maximum=1000, step=1, label="Top K", value=0)
         top_p_slider = gr.Slider(minimum=0, maximum=1, step=0.01, label="Top P", value=1)
         best_of_slider = gr.Slider(minimum=1, maximum=1000, step=1, label="Best Of, for beam search", value=3)
+        return_prompt_box = gr.Checkbox(label="Return Prompt", value=False)
 
         self.iface = gr.Interface(
             fn=lambda *args: self.to_html_str(*self.gen_and_cal_prob(*args)),
-            inputs=["text", max_new_token_slider, temperature_slider, use_beam_search_box, top_k_slider, top_p_slider, best_of_slider],
-            outputs=["html", "html"],
+            inputs=[
+                "text",
+                max_new_token_slider,
+                temperature_slider,
+                use_beam_search_box,
+                top_k_slider,
+                top_p_slider,
+                best_of_slider,
+                return_prompt_box,
+            ],
+            outputs=[gr.HTML("Probability", show_label=True), gr.HTML("Entropy", show_label=True), gr.HTML("Stats")],
             theme="default",
             title="Visualize LLMs",
             description=f"The color visualize the log-probabilities of the generated tokens, from red (low probability) to green (high probability). Should expect the model to generate low probability if it feels uncertain. The model used is {model_name}.",
         )
 
-    def prepare_input(self, text) -> dict:
-        input_tensor = self.tokenizer(text)
-        return input_tensor
-
-    def get_logprob(self, input_ids, output_ids):
+    def get_logprob_and_entropy(self, input_ids, output_ids):
         """
         input_ids: [batch_size, seq_len]
-        output_ids: [batch_size, seq_len]"""
+        output_ids: [batch_size, seq_len]
+        return: [{token_id: logprob}, ...]"""
         num_gen_tokens = output_ids.shape[-1] - input_ids.shape[-1]
         # print(num_gen_tokens)
         assert torch.eq(output_ids[0][: input_ids.shape[-1]], input_ids[0]).all()
@@ -57,14 +67,15 @@ class VisualizeLLM:
         logits = self.llm(**input_kwargs).logits
         assert logits.shape[1] == input_ids.shape[-1] + num_gen_tokens
         logprobs = logprobs_from_logits(logits[:, :-1, :], input_kwargs["input_ids"][:, 1:])
-        ls = []
-        for token, logprob in zip(output_ids[0][-num_gen_tokens:], logprobs[0][-num_gen_tokens:]):
-            d = {}
-            d[token.item()] = logprob.item()
-            ls.append(d)
-        return ls
+        entropys = entropy_from_logits(logits[:, :-1, :])
+        logprob_ls = []
+        entropy_ls = []
+        for token, logprob, entropy in zip(output_ids[0][-num_gen_tokens:], logprobs[0][-num_gen_tokens:], entropys[0][-num_gen_tokens:]):
+            logprob_ls.append({token.item(): logprob.item()})
+            entropy_ls.append({token.item(): entropy.item()})
+        return logprob_ls, entropy_ls
 
-    def cal_prob(self, logprobs):
+    def cal_stats(self, logprobs, entropys=None):
         """logprobs = [{token_id: logprob}, ...}]"""
         ls = []
         for logprob in logprobs:
@@ -72,13 +83,17 @@ class VisualizeLLM:
                 ls.append(
                     {
                         "text": self.tokenizer.convert_ids_to_tokens(token_id).replace("▁", " ").replace("<0x0A>", "<|new_line|>"),
-                        "value": math.exp(logprob),
+                        "prob": math.exp(logprob),
                     }
                 )
+        if entropys is not None:
+            for pb, entropy in zip(ls, entropys):
+                for token_id, ep in entropy.items():
+                    pb["entropy"] = ep
         return ls
 
-    def gen_and_cal_prob(self, text, max_new_tokens, temperature, use_beam_search, top_k, top_p, best_of):
-        stats = "<b>Stats:</b><br>"
+    def gen_and_cal_prob(self, text, max_new_tokens, temperature, use_beam_search, top_k, top_p, best_of, return_prompt):
+        logs = "<b>Stats:</b><br>"
         input_text = [self.chat_template(text)]
         input_ids = self.tokenizer(input_text)["input_ids"]
         if self.use_vllm:
@@ -97,8 +112,8 @@ class VisualizeLLM:
             start = time.time()
             outputs = self.llm.generate(prompt_token_ids=input_ids, sampling_params=sampling_params)[0].outputs[0]
             generate_time = time.time() - start
-            stats += f"generate time: {generate_time:.2f} s<br>"
-            probs = self.cal_prob(outputs.logprobs)
+            logs += f"generate time: {generate_time:.2f} s<br>"
+            values = self.cal_stats(outputs.logprobs)
         else:
             input_ids = torch.tensor(input_ids).to("cuda")
             input_kwargs = {
@@ -114,29 +129,38 @@ class VisualizeLLM:
             start = time.time()
             output_ids = self.llm.generate(**input_kwargs)
             generate_time = time.time() - start
-            stats += f"generate time: {generate_time:.2f} s<br>"
+            logs += f"generate time: {generate_time:.2f} s<br>"
 
             start = time.time()
-            logprobs = self.get_logprob(input_ids, output_ids)
-            stats += f"cal logp time: {time.time() - start:.2f} s<br>"
+            logprobs, entropys = self.get_logprob_and_entropy(input_ids, output_ids)
+            logs += f"cal logs time: {time.time() - start:.2f} s<br>"
 
-            probs = self.cal_prob(logprobs)
+            values = self.cal_stats(logprobs=logprobs, entropys=entropys)
 
-        stats += f"num tokens: {len(probs)}<br>"
-        stats += f"num tokens per second: {len(probs) / generate_time:.2f}<br>"
+        logs += f"num tokens: {len(values)}<br>"
+        logs += f"num tokens per second: {len(values) / generate_time:.2f}<br>"
 
-        return probs, stats
+        return logs, values
 
-    def to_html_str(self, data, stats: str) -> [str, str]:
-        """data is a list of dictionaries like [{'text': 'Hello', 'value': 0.5}, ...]"""
-        colored_text = ""
-        legend_info = stats + "<br><b>Legend:</b><br>"
-        for item in data:
-            background_color = color_map(item["value"])
+    def to_html_str(self, logs: str, values) -> [str, str]:
+        """values is a list of dictionaries like [{'text': 'Hello', 'prob': 0.5, 'entropy'}, ...]"""
+        keys = [key for key in values[0].keys() if key != "text"]
+
+        colored_text = [""] * len(keys)
+        # add legends to colored_text
+        for i, key in enumerate(keys):
+            colored_text[i] += f"<strong>{key}:</strong><br>"
+        log_info = logs + "<br><b>Legend:</b><br>"
+        for item in values:
             text = item["text"] if item["text"] != "<|new_line|>" else " <br>"
-            colored_text += f"<span style='background-color: {background_color}; padding: 0px;'>{text}</span>"
-            legend_info += f"{item['text']}: Probability: {item['value']:.2f}, Color: {background_color}<br>"
-        return colored_text, legend_info
+            log_info += f"{text}: "
+            for i, key in enumerate(keys):
+                print(key, item[key])
+                background_color = color_map(item[key], key)
+                colored_text[i] += f"<span style='background-color: {background_color}; padding: 0px;'>{text}</span>"
+                log_info += f"{key}: <span style='background-color: {background_color}; padding: 0px;'>{item[key]:.2f}</span>, "
+            log_info += "<br>"
+        return colored_text[0], colored_text[1], log_info
 
     def run(self):
         self.iface.launch(share=True)
@@ -146,5 +170,5 @@ if __name__ == "__main__":
     model_name = "openchat/openchat_3.5"
     # model_name = "lvwerra/gpt2-imdb"
     extra_generate_kwargs = {}
-    server = VisualizeLLM(model_name, extra_generate_kwargs, openchat_template, use_vllm=False)
+    server = VisualizeLLM(model_name, extra_generate_kwargs, openchat_template)
     server.run()
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
+import colorsys
+import math
+
 import torch
 from torch.nn import functional as F
-import colorsys
+
 
 def logprobs_from_logits(logits, labels, gather=True):
     """
@@ -14,10 +17,25 @@ def logprobs_from_logits(logits, labels, gather=True):
     return logpy
 
 
-def color_map(value):
-    # Assuming value is between 0 and 1
-    # Hue: 0 (red) to 120 (green) in HSV color space
-    hue = value * 120  # Green to Red
+def entropy_from_logits(logits):
+    """Calculate entropy from logits."""
+    pd = torch.nn.functional.softmax(logits, dim=-1)
+    entropy = torch.logsumexp(logits, axis=-1) - torch.sum(pd * logits, axis=-1)
+    return entropy
+
+
+def color_map(value, key=None):
+    if key == "prob":
+        # Assuming value is between 0 and 1
+        # Hue: 0 (red) to 120 (green) in HSV color space
+        hue = value * 120  # Green to Red
+    elif key == "entropy":
+        # Assuming value is between 0 and log(25000)
+        # Hue: 0 (red) to 120 (green) in HSV color space
+        hue = max((1 - (value / 2) * 120, 0))
+    else:
+        raise ValueError("key should be 'prob' or 'entropy'")
+
     saturation = 0.5  # Full saturation
     brightness = 1  # Full brightness
 
@@ -25,5 +43,9 @@ def color_map(value):
     r, g, b = colorsys.hsv_to_rgb(hue / 360, saturation, brightness)
     return f"rgb({int(r * 255)}, {int(g * 255)}, {int(b * 255)})"
 
+
 def openchat_template(text: str) -> str:
-    return "GPT4 Correct User: " + text + "<|end_of_turn|>GPT4 Correct Assistant:"
+    """Template for OpenChat
+    There shouldn't be spaces at the end of the string, otherwise the model will generate list, e.g.:
+    1. ..., 2. ..., 3. ..."""
+    return "GPT4 Correct User: " + text + "<|end_of_turn|> GPT4 Correct Assistant:"
